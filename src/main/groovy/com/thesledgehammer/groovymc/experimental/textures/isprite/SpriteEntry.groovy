0bc667d7af7c2551e8f72f4c2bf5c1b304199376
BanjoSledge <martin.kelly4000@gmail.com>
2019-03-26 15:01:44 +1100

    ISprites

--- src/main/groovy/com/thesledgehammer/groovymc/experimental/textures/isprite/SpriteEntry.groovy
+++ src/main/groovy/com/thesledgehammer/groovymc/experimental/textures/isprite/SpriteEntry.groovy
@@ -2,6 +2,7 @@ package com.thesledgehammer.groovymc.experimental.textures.isprite
 
 import com.thesledgehammer.groovymc.api.ISprite
 import com.thesledgehammer.groovymc.client.definitions.GroovyAtlasSpriteDefinition
+import com.thesledgehammer.groovymc.utils.SpriteTools
 import net.minecraft.client.renderer.texture.TextureAtlasSprite
 import net.minecraft.client.renderer.texture.TextureMap
 import net.minecraft.util.ResourceLocation
@@ -16,6 +17,7 @@ class SpriteEntry implements ISprite {
 
     SpriteEntry(ResourceLocation spriteLocation) {
         this.spriteLocation = spriteLocation;
+        this.sprite = GroovyAtlasSpriteDefinition.createForConfig(spriteLocation);
     }
 
     void onTextureStitchPre(TextureMap map) {
@@ -29,16 +31,16 @@ class SpriteEntry implements ISprite {
 
     @Override
     void bindTexture() {
-
+        SpriteTools.BindBlockTextureMap();
     }
 
     @Override
     double getInterpU(double u) {
-        return 0
+        return sprite.getInterpolatedU(u * 16);
     }
 
     @Override
     double getInterpV(double v) {
-        return 0
+        return sprite.getInterpolatedV(v * 16);
     }
 }
